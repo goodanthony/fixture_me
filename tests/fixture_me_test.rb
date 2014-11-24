eb66bd9fa74d96e4855bd4021bac4f2328b3ed68
anthony <anthony@iamfree.com>
2014-11-24 14:19:21 +1100

    remove old directory

--- tests/fixture_me_test.rb
+++
@@ -1,9 +0,0 @@
-require File.join(File.dirname(__FILE__), '..', 'test_helper.rb')
-class FitureMeTest < Minitest::Test
-
-
-   def test_that_life_is_good
-      assert_equal "hello world",  "hello world"
-    end
-
-end
